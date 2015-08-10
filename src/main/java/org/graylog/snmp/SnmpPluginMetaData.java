06b8850357b4677f24100c8d2072466c3c285d26
Bernd Ahlers <bernd@graylog.com>
2015-08-10 16:08:51 +0200

    Fix version in SnmpPluginMetaData

--- src/main/java/org/graylog/snmp/SnmpPluginMetaData.java
+++ src/main/java/org/graylog/snmp/SnmpPluginMetaData.java
@@ -34,7 +34,7 @@ public class SnmpPluginMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 0, 0);
+        return new Version(0, 2, 1);
     }
 
     @Override
