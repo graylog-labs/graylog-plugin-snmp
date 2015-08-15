5cef93d461a4fb8369a620041600f91f368ab732
Bernd Ahlers <bernd@graylog.com>
2015-08-15 11:05:19 +0200

    Prefix all SNMP field names with "snmp_"
    Also use the PDU type string instead of the numeric value.

--- src/main/java/org/graylog/snmp/SnmpCommandResponder.java
+++ src/main/java/org/graylog/snmp/SnmpCommandResponder.java
@@ -16,6 +16,8 @@ import org.snmp4j.smi.VariableBinding;
 public class SnmpCommandResponder implements CommandResponder {
     private static final Logger LOG = LoggerFactory.getLogger(SnmpCommandResponder.class);
 
+    private static final String KEY_PREFIX = "snmp_";
+
     private final RawMessage rawMessage;
     private Message message = null;
 
@@ -27,6 +29,10 @@ public class SnmpCommandResponder implements CommandResponder {
         return message;
     }
 
+    private String withKeyPrefix(String key) {
+        return KEY_PREFIX + key;
+    }
+
     @Override
     public void processPdu(CommandResponderEvent event) {
         LOG.debug("Processing SNMP event: {}", event);
@@ -35,8 +41,8 @@ public class SnmpCommandResponder implements CommandResponder {
         final Integer32 requestID = pdu.getRequestID();
         final Message message = new Message("SNMP trap " + requestID.toString(), null, rawMessage.getTimestamp());
 
-        message.addField("trap_type", pdu.getType());
-        message.addField("request_id", requestID.toLong());
+        message.addField(withKeyPrefix("trap_type"), PDU.getTypeString(pdu.getType()));
+        message.addField(withKeyPrefix("request_id"), requestID.toLong());
 
         for (final VariableBinding binding : pdu.getVariableBindings()) {
             final String key = decodeOid(binding.getOid());
@@ -44,12 +50,12 @@ public class SnmpCommandResponder implements CommandResponder {
 
             try {
                 if (variable instanceof TimeTicks) {
-                    message.addField(key, ((TimeTicks) variable).toMilliseconds());
+                    message.addField(withKeyPrefix(key), ((TimeTicks) variable).toMilliseconds());
                 } else {
-                    message.addField(key, variable.toLong());
+                    message.addField(withKeyPrefix(key), variable.toLong());
                 }
             } catch (UnsupportedOperationException e) {
-                message.addField(key, variable.toString());
+                message.addField(withKeyPrefix(key), variable.toString());
             }
         }
 
